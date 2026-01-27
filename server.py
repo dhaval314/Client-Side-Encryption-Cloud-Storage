26cc375717139fb0ca1ee97ca927b808f0117a6c
dhaval314 <bondedhaval@gmail.com>
2026-01-27 17:20:32 +0530

    upload functionality added, storage path is hardcoded for now

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi import UploadFile, File
+import aiofiles
 
 app = FastAPI()
 
-@app.get("/upload")
-async def upload():
-    return {"message":"upload page"}
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    file_path = "/home/dhaval/storage/"
+    
+    async with aiofiles.open(f"{file_path}/{file.filename}", mode= "wb") as f:
+        while content := await file.read(1024):
+            await f.write(content)
+    return {
+        "filename":file.filename,
+        "file_size":file.size
+    }
+
 
 
 @app.get("/download")

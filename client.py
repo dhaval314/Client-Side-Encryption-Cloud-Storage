857a3d2f3d0ab41e02ee8390ff4a7d0ab433e062
dhaval314 <bondedhaval@gmail.com>
2026-01-27 22:01:11 +0530

    added download functionality and fixed client side error handling

--- client.py
+++ client.py
@@ -1,13 +1,32 @@
 import requests
 
+file_path = "/home/dhaval/Desktop/CTF tools"
 upload_endpoint = "http://localhost:8000/upload"
+download_endpoint = "http://localhost:8000/download"
+download_path = "/home/dhaval/Downloads"
 
-with open("/home/dhaval/Desktop/test", "rb") as f:
-    files = {
-        "file": ("test", f)
-    }
 
-    r = requests.post(upload_endpoint, files=files)
+def upload(user_id):
+    with open(f"{file_path}", "rb") as f:
+        files = {
+            "file": (file_path.split("/")[-1], f)
+        }
+        r = requests.post(upload_endpoint + "/" + str(user_id), files=files)
 
-print(r.status_code)
-print(r.text)
+    print(r.status_code)
+    print(r.text)
+
+def download(user_id, file_id, download_path):
+    try:
+        response = requests.get(f"{download_endpoint}/{user_id}/{file_id}", stream=True)
+        with open(f"{download_path}/{file_id}", "wb") as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                f.write(chunk)
+        print(response.status_code)
+        print(response.text)
+    except Exception as e:
+        print(f"Error: {e}")
+    
+    
+
+upload(5)
--- server.py
+++ server.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI
-from fastapi import UploadFile, File
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
 import aiofiles
+import aiofiles.os
+import os
+import getpass
+
 
 app = FastAPI()
 
-@app.post("/upload")
-async def upload(file: UploadFile = File(...)):
-    file_path = "/home/dhaval/storage/"
-    
-    async with aiofiles.open(f"{file_path}/{file.filename}", mode= "wb") as f:
+username = getpass.getuser()
+storage_path = "/home/dhaval/storage"
+download_path = f"/home/{username}/Downloads"
+
+@app.post("/upload/{user_id}")
+async def upload(user_id, file: UploadFile = File(...)):
+
+    try:
+        await aiofiles.os.makedirs(f"{storage_path}/{user_id}")
+    except:
+        print(f"Directory for user: {user_id} already exists")
+
+
+    async with aiofiles.open(f"{storage_path}/{user_id}/{file.filename}", mode= "wb") as f:
         while content := await file.read(1024):
             await f.write(content)
     return {
@@ -18,6 +31,15 @@ async def upload(file: UploadFile = File(...)):
 
 
 
-@app.get("/download")
-async def download():
-    return {"message":"download page"}
+@app.get("/download/{user_id}/{file_id}")
+async def download(user_id, file_id):
+
+    def file_iterator():
+        with open(f"{storage_path}/{user_id}/{file_id}", "rb") as file:
+            while chunk := file.read(1024):
+                yield chunk
+    
+    return StreamingResponse(
+        file_iterator()
+    )
+        
